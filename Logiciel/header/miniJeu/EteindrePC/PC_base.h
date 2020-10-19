ac07bf25fbaba2c134b8c070035611d9f33b880f
erwann-lubrano <61875357+erwann-lubrano@users.noreply.github.com>
2020-10-19 09:45:14 +0200

    des commentaires par ci des commentaires par la

--- Logiciel/header/miniJeu/EteindrePC/PC_base.h
+++ Logiciel/header/miniJeu/EteindrePC/PC_base.h
@@ -10,26 +10,93 @@
 #include "Personnage.h"
 
 namespace EteindrePC{
+	
+	/**
+	*  @author Erwann Lubrano
+	*  la class PC_base est une class abstraite permettant 
+	*/
 	class PC_base{
+		
+		
 		private :
+		
 			sf::Clock clock;
+			
 			bool notFirstTime;
+			
+			
 		protected :
+		
 			sf::Vector2f position;
+			
 			bool on;
+			
 			std::pair<sf::Vector2f*,EteindrePC::Direction*> persoView;
+			
+			/**
+			*  @brief permet de savoir si le Personnage regarde le PC.
+			*  @return renvoie true si le personnage regarde le PC sinon renvoie false
+			*/
 			virtual bool isPersonnageLookingAt()=0;
+			
+			/**
+			*  @brief dessine le PC quand il est allumé
+			*  @param window : la feunetre où afficher le PC
+			*/
 			virtual void drawWhenIsOn(sf::RenderWindow& window)=0;
+			
+			/**
+			*  @brief dessine le PC quand il est éteint
+			*  @param window : la feunetre où afficher le PC
+			*/
 			virtual void drawWhenIsOff(sf::RenderWindow& window)=0;
 			
 		public :
+		
+			/**
+			*  @brief getter de on
+			*  @return renvoie true si le PC est allumé sinon renvoie false.
+			*/
 			bool isOn() const;
+			
+			/**
+			*  @brief setter de on
+			*  @param b : true si le PC doit etre allumé, sinon false.
+			*/
 			void setOn(bool b);
+			
+			/**
+			*  @brief actualise l'etat du PC
+			*/
 			void update();
+			
+			/**
+			*  @brief affiche le PC
+			*  @param window : la feunetre où le PC doit etre allumé
+			*/
 			void draw(sf::RenderWindow& window);
+			
+			/**
+			*  @brief setter de persoView
+			*  @param pView : une paire contenant la position (first), position(second)
+			*/
 			void setPersoView(const std::pair<sf::Vector2f*,EteindrePC::Direction*>& pView);
+			
+			/**
+			*  @brief setter de position
+			*  @param pos : position voulue.
+			*/
 			void setPosition(const sf::Vector2f& pos);
+			
+			/**
+			*  @brief constructeur de PC_base
+			*/
 			PC_base();
+			
 	};
+	
+	
 }
+
+
 #endif
--- Logiciel/header/miniJeu/EteindrePC/Personnage.h
+++ Logiciel/header/miniJeu/EteindrePC/Personnage.h
@@ -8,6 +8,11 @@
 #include "CollisionBox.h"
 
 namespace EteindrePC{
+	
+	/**
+	*  @author Erwann Lubrano
+	*  L'enumeration Direction sert à connaitre la direction vers laquelle le Personnage est tourné.
+	*/
 	enum Direction{
 		Bas=0,
 		Gauche=1,
@@ -15,22 +20,64 @@ namespace EteindrePC{
 		Haut=3
 	};
 	
+	/**
+	*  @author Erwann Lubrano
+	*  La class Personnage permet de créer un Personnage controlable avec les touche Z,Q,S,D.
+	*/
 	class Personnage{
+		
+		
 		private :
+		
 			sf::Vector2f position;
+			
 			sf::Sprite sprite;
+			
 			sf::Clock clock;
+			
 			Direction direction;
+			
 			sf::Texture texture;
 			unsigned char sprite_x;
+			
 			std::vector<CollisionBox*> collisionList;
 			
 		public :
+		
+			/**
+			*  @brief constructeur 
+			*  Constructeur de Personnage
+			*/
 			Personnage();
+			
+			/**
+			*  @brief fonction permettant de deplacer (avec les touches Z,Q,S,D) le Personnage si possible et de changé sa direction.
+			*/
 			void update();
+			
+			/**
+			*  @brief fonction permettant d'afficher le Personnage
+			*  @param window la feunetre dans laquelle le Personnage doit etre affiché
+			*/
 			void draw(sf::RenderWindow& window);
+			
+			/**
+			*  @brief ajoute une zone ou le joueur ne pourra pas aller.
+			*  @param collisionBox : boite de collision. 
+			*/
 			void addCollisionBox(CollisionBox& collisionBox);
+			
+			/**
+			*  @brief recupère la position et la direction du Personnage
+			*  @return la pair renvoyée contient les adresses de la position (first) et de la direction (second) 
+			*/
 			std::pair<sf::Vector2f*,EteindrePC::Direction*> getPersoView();
+			
+			
 	};
+	
+	
 }
+
+
 #endif
