1d54f0528b39f8fc9ebfb8f5a4e6e43d3f82957d
erwann-lubrano <61875357+erwann-lubrano@users.noreply.github.com>
2020-10-21 14:09:21 +0200

    Des commentaires

--- Logiciel/header/miniJeu/EteindrePC/PC_haut.h
+++ Logiciel/header/miniJeu/EteindrePC/PC_haut.h
@@ -5,13 +5,39 @@
 #include "PC_base.h"
 
 namespace EteindrePC{
+	
+	/**
+	*  @author Erwann Lubrano
+	*  PC_haut est la class des PC que le Personnage pourra eteindre/allumé quand il est tourné vers le haut.
+	*/
 	class PC_haut : public PC_base{
+		
 		private :
+			
+			/**
+			*  @brief permet de savoir si le Personnage regarde le PC.
+			*  @return renvoie true si le personnage regarde le PC sinon renvoie false
+			*/
 			bool isPersonnageLookingAt();
+			
+			/**
+			*  @brief dessine le PC quand il est allumé
+			*  @param window : la feunetre où afficher le PC
+			*/
 			void drawWhenIsOn(sf::RenderWindow& window);
+			
+			/**
+			*  @brief dessine le PC quand il est éteint
+			*  @param window : la feunetre où afficher le PC
+			*/
 			void drawWhenIsOff(sf::RenderWindow& window);
 			
 		public :
+		
 	};
+	
+	
 }
+
+
 #endif
