561cb1c931fcd6372badbb0d6380e12cd987cc0a
Quentin <quentin.roubin@etu.univ-amu.fr>
2020-11-03 14:03:39 +0100

    Modifs
    Minijeu.h/.cpp pour ajouter l'interface

--- Logiciel/source/miniJeu/MiniJeu.cpp
+++ Logiciel/source/miniJeu/MiniJeu.cpp
@@ -16,6 +16,7 @@ void MiniJeu::play(){
 		}
 		app.window.clear(backgroundColor);
 		this->update();
+        drawInterface();
 		this->draw();
 		app.window.display();
 	}
@@ -35,4 +36,29 @@ sf::Color MiniJeu::getBackgroundColor()const{
 
 void MiniJeu::setBackgroundColor(const sf::Color& color){
 	backgroundColor = color;
+}
+
+void MiniJeu::drawInterface(){
+    if (!coeur.loadFromFile("../ressource/coeur.png"))
+    {
+        std::cout<<"erreur loadFromFile"<<std::endl;
+    }
+    sf::Text texteScore;
+    texteScore.setFont(app.font);
+    texteScore.setString("Score: "+std::to_string(app.score));
+    texteScore.setPosition(app.window.getSize().x-150,app.window.getSize().y-30);
+    sf::Sprite sprite1, sprite2, sprite3;
+    sprite1.setTexture(coeur);
+    sprite1.setScale(0.1f, 0.1f);
+    sprite1.setPosition(sf::Vector2f(0,0));
+    sprite2.setTexture(coeur);
+    sprite2.setScale(0.1f, 0.1f);
+    sprite2.setPosition(sf::Vector2f(40,0));
+    sprite3.setTexture(coeur);
+    sprite3.setScale(0.1f, 0.1f);
+    sprite3.setPosition(sf::Vector2f(80,0));
+    if (app.lives>=1) app.window.draw(sprite1);
+    if (app.lives>=2) app.window.draw(sprite2);
+    if (app.lives>=3) app.window.draw(sprite3);
+    app.window.draw(texteScore);
 }
