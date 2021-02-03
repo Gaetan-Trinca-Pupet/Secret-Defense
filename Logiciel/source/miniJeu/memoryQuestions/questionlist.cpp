2eb03838adb53605b34b14c32b8c8a2025dd357d
Nelirks <raphael.lesbros@laposte.net>
2021-02-03 09:24:00 +0100

    Séparation Strings_Code de MemoryQuestions

--- Logiciel/source/miniJeu/memoryQuestions/questionlist.cpp
+++ Logiciel/source/miniJeu/memoryQuestions/questionlist.cpp
@@ -40,69 +40,17 @@ std::string QuestionList::getQuestion(const unsigned short & rank)
         std::cerr << "Question non disponible";
         return "";
     }
-    switch (rank) {
-    case 0 :
-        return "Est ce que je peux faire un cin ?";
-        break;
-    case 1 :
-        return "Est ce que je peux faire un goto ?" ;
-        break;
-    case 2 :
-        return "Vous pouvez repeter la question ?" ;
-        break;
-    case 3 :
-        return "Je ne comprend pas pourquoi mon code ne marche pas ici." ;
-        break;
-    case 4 :
-        return "Pourquoi mon code ne marche pas ?" ;
-        break;
-    case 5 :
-        return "Pourquoi vous avez mis cette condition ici ?" ;
-        break;
-    case 6 :
-        return "Est ce qu'on aurait pas pu inverser les conditions ?" ;
-        break;
-    case 7 :
-        return "Quelle est la difference entre & et && ?" ;
-        break;
-    case 8 :
-        return "Quelle est la difference entre | et || ?" ;
-        break;
-    case 9 :
-        return "Est ce que je peux faire un cout ?" ;
-        break;
-    case 10 :
-        return "Je peux utiliser i++ ici ?";
-        break;
-    case 11 :
-        return "Je peux utiliser ++i ici ?";
-        break;
-    case 12 :
-        return "Quelle est la difference entre ++i et i++";
-        break;
-    case 13 :
-        return "Quelle est la difference entre un pointeur et une reference ?";
-        break;
-    case 14 :
-        return "Quelle est la difference entre * et &";
-        break;
-    case 15 :
-        return "Pourquoi on utilise const & ?";
-        break;
-    case 16 :
-        return "Est ce qu'on utilise la recursivite ?";
-        break;
-    case 17 :
-        return "Comment marche la recursivite ?";
-        break;
-    case 18 :
-        return "On peut utiliser nos PC pour programmer ?";
-        break;
-    case 19 :
-        return "Sur quoi sera le prochain controle ?";
-        break;
+    std::fstream questionFile;
+    questionFile.open("../ressource/MemoryQuestions/QuestionList.txt",std::ios::in);
+    if (questionFile.is_open()) {
+        std::string question;
+        for (unsigned i = 0; i < rank; ++i)
+            std::getline(questionFile, question);
+        return question;
     }
-	return ""; 
+    else {
+        return "CAMARCHEPAS";
+    }
 }
 
 unsigned short QuestionList::getRandomQuestion()
