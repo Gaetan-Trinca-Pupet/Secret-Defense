b8d5b2d1add26058f4247038eacc6944ce48b60d
Ugo-Larsonneur <ugo.larsonneur@etu.univ-amu.fr>
2021-02-07 23:37:34 +0100

    correction de ma stupidité

--- Logiciel/source/miniJeu/Bronx/Bronx.cpp
+++ Logiciel/source/miniJeu/Bronx/Bronx.cpp
@@ -7,43 +7,47 @@ void Bronx::Bronx::setup()
     {
         //TODO: remplacer 666 par des vraies valeur de position
         verres.push_back(new Deliverable(666 + i * 666, 666, 42, 72));
-        verres[0]->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeVerVide"));
+        verres[i]->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeVerVide"));
     }
 
     for (unsigned int i = 0; i < 2; ++i)
     {
+        
         //TODO: remplacer 666 par des vraies valeur de position et taille
-        ingredients.push_back(new Ingredient(true));
-        verres[i]->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeVerDeGin"));
-        verres[i]->setSize(sf::Vector2f(666,666));
+        Ingredient* tmpIngredient= new Ingredient(true);
+        ingredients.push_back(tmpIngredient);
+        tmpIngredient->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeVerDeGin"));
+        tmpIngredient->setSize(sf::Vector2f(666,666));
     }
 
     for (unsigned int i = 0; i < 4; ++i)
     {
         //TODO: remplacer 666 par des vraies valeur de position
-        ingredients.push_back(new Ingredient(true));
-        verres[i]->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeVerDeJusDOrange"));
-        verres[i]->setSize(sf::Vector2f(666, 666));
+        Ingredient* tmpIngredient = new Ingredient(true);
+        ingredients.push_back(tmpIngredient);
+        tmpIngredient->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeVerDeJusDOrange"));
+        tmpIngredient->setSize(sf::Vector2f(666, 666));
     }
 
     for (unsigned int i = 0; i < 19; ++i)
     {
         //TODO: remplacer 666 par des vraies valeur de position
-        ingredients.push_back(new Ingredient(false));
+        Ingredient* tmpIngredient = new Ingredient(true);
+        ingredients.push_back(tmpIngredient);
 
         switch (rand() % 777)
         {
             case 0:
-                verres[i]->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeTrucPasComestible0"));
-                verres[i]->setSize(sf::Vector2f(666, 666));
+                tmpIngredient->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeTrucPasComestible0"));
+                tmpIngredient->setSize(sf::Vector2f(666, 666));
                 break;
             case 1:
-                verres[i]->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeTrucPasComestible1"));
-                verres[i]->setSize(sf::Vector2f(666, 666));
+                tmpIngredient->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeTrucPasComestible1"));
+                tmpIngredient->setSize(sf::Vector2f(666, 666));
                 break;
             case 2:
-                verres[i]->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeTrucPasComestible2"));
-                verres[i]->setSize(sf::Vector2f(666, 666));
+                tmpIngredient->setTexture(&AssetManager::getTexture("cheminVersLeSpriteDeTrucPasComestible2"));
+                tmpIngredient->setSize(sf::Vector2f(666, 666));
                 break;
         }
         
