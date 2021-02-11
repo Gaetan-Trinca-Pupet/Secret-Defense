5af73cd9170bb097ef4e9da909c791ec494b88d4
Nelirks <raphael.lesbros@laposte.net>
2021-02-11 14:15:43 +0100

    Ajout de la description de gifle (utf 8 non fonctionnel)

--- Logiciel/main.cpp
+++ Logiciel/main.cpp
@@ -64,8 +64,8 @@ int main()
     sf::Texture textureBg;
 
     MiniJeuManager manager(app);
-    manager.addMiniJeu(createGifle, "Gifle", "DescriptionGifle");
-    manager.play();
+    manager.addMiniJeu(createGifle, "Gifle", "Giflez les étudiants ne portant pas de masque !\nCOntrôles : Souris");
+    manager.play("Gifle");
 
     return 0;
 }
