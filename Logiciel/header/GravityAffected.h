e5328b6f3ffba4a7bd4792e93bcc2af63d5d191d
Ugo-Larsonneur <ugo.larsonneur@etu.univ-amu.fr>
2021-02-12 16:07:27 +0100

    création de la classe GravityAffected

---
+++ Logiciel/header/GravityAffected.h
@@ -0,0 +1,22 @@
+#ifndef __GRAVITYAFFECTED_H_
+#define __GRAVITYAFFECTED_H_
+
+class GravityAffected
+{
+public:
+	GravityAffected();
+	~GravityAffected();
+
+private:
+
+};
+
+GravityAffected::GravityAffected()
+{
+}
+
+GravityAffected::~GravityAffected()
+{
+}
+
+#endif
